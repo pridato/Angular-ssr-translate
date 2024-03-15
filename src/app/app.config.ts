119642b6cb5e9670b72d24b2c9e616aea4f98492
pridato <davidarroyo25052004@gmail.com>
2024-03-15 13:04:23 +0100

    angular translate on ssr project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return  new  TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient],
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    importProvidersFrom([
+      HttpClientModule, 
+      TranslateModule.forRoot(provideTranslation())
+    ]),
+    provideRouter(routes),
+  ]
 };
---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+}
